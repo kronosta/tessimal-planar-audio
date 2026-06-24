7aa0d060657b7a595e611ba5b51ea7b7bd2ad6ed
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2026-06-24 14:58:30 -0700

    song slicing

--- planar_audio.gd
+++ planar_audio.gd
@@ -3,11 +3,23 @@ class_name PlanarAudio extends Node2D
 @export var song: Texture2D
 
 func _ready():
-	var sound_file = FileAccess.open("res://long_sound.raw", FileAccess.WRITE)
+	var sound_file = FileAccess.open("res://song.raw", FileAccess.WRITE)
 	
-	for i in 44100 * 12:
+	var song_image := song.get_image()
+	var seconds_in_a_pixel := 0.25
+	
+	var angle := TAU / 8.0
+	
+	var offset := Vector2.ZERO
+	
+	for i in 44100:
 		var time := float(i) / 44100.0
 		
-		sound_file.store_16(sin(time * TAU * 720.0) * 4096.0)
+		var magnitude := song_image.get_pixel(
+			int((time / seconds_in_a_pixel) + offset.x) * cos(angle),
+			int((time / seconds_in_a_pixel) + offset.y) * sin(angle)
+		).r
+		
+		sound_file.store_16(sin(time * TAU * 720.0) * magnitude * 4096.0)
 	
 	sound_file.close()
